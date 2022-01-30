ea1d9da0cafd331b77692b9f7ce892056f9af43a
theNamskov <ahoryaw1@outlook.com>
2022-01-30 21:01:31 +0000

    added boilerplate actors for implementing simple state mgmt

---
+++ redux/counter/app.js
@@ -0,0 +1,13 @@
+const state = null
+
+const listeners = []
+
+// TODO: Define callbacks for subscription
+
+const subscribe = (cb) => { }
+
+const reducer = (currState, action) => { }
+ 
+const dispatch = (action) => { }
+
+// TODO: Set up click listener callbacks for increment and decrement buttons
