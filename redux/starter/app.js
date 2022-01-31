23473d41e736e97e0e2e7cf69fc08dc4573a6446
theNamskov <ahoryaw1@outlook.com>
2022-01-31 23:29:03 +0000

    added initial snippets for recipe app logic

--- redux/starter/app.js
+++ redux/starter/app.js
@@ -1,16 +1,10 @@
 import { createStore } from 'redux';
 
-const reducer = (state, action) =>
-	action.type === 'INC'
-		? state + 1
-		: state;
 
-const initialState = 0;
+const reducer = (state, action) => state
 
-const store = createStore(reducer, initialState);
 
-store.subscribe(() => document.getElementById('counter').innerText = store.getState());
+const store = createStore(reducer)
 
-setInterval(() => store.dispatch({ type: 'INC' }), 500);
 
-console.log("Redux started");
+window.store = store
