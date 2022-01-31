aa7e512f623fcb96cbecc8688604c74a67914980
theNamskov <ahoryaw1@outlook.com>
2022-01-31 23:35:15 +0000

    provided initial app state as store enhancer

--- redux/starter/app.js
+++ redux/starter/app.js
@@ -4,7 +4,23 @@ import { createStore } from 'redux';
 const reducer = (state, action) => state
 
 
-const store = createStore(reducer)
+const initialState = {
+	recipes: [
+		{
+			name: 'Omelette',
+		},
+	],
+
+	ingredients: [
+		{
+			recipe: 'Omelette',
+			name: 'Egg',
+			quantity: 2,
+		},
+	]
+}
+
+const store = createStore(reducer, initialState)
 
 
 window.store = store
