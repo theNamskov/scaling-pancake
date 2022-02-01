0e3257f622d7927b467233d9e108cfc947e74cdb
theNamskov <ahoryaw1@outlook.com>
2022-02-01 02:43:04 +0000

    employed combineReducers in building root reducer

--- redux/starter/reducers/root.js
+++ redux/starter/reducers/root.js
@@ -1,8 +1,9 @@
+import { combineReducers } from 'redux'
 
 import recipesReducer from './recipes'
 import ingredientsReducer from './ingredients'
 
-export default (state, action) => Object.assign({}, state, {
-    recipes: recipesReducer(state.recipes, action),
-    ingredients: ingredientsReducer(state.ingredients, action),
+export default combineReducers({
+    recipes: recipesReducer,
+    ingredients: ingredientsReducer,
 })
