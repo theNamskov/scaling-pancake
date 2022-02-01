cdb07e0a4963e3389f6b3b9700dede061169e94a
theNamskov <ahoryaw1@outlook.com>
2022-02-01 02:31:06 +0000

    imported separate reducers into root reducer

--- redux/starter/reducers/root.js
+++ redux/starter/reducers/root.js
@@ -1,18 +1,8 @@
-import { ADD_RECIPE } from "../actions/recipes"
-import { ADD_INGREDIENT } from "../actions/ingredients"
 
-export default (state, action) => {
-	switch(action.type) {
-		case ADD_RECIPE:
-			return Object.assign({}, state, { 
-                recipes: state.recipes.concat(action.payload) 
-            })
+import recipesReducer from './recipes'
+import ingredientsReducer from './ingredients'
 
-		case ADD_INGREDIENT:
-			return Object.assign({}, state, { 
-                ingredients: state.ingredients.concat(action.payload) 
-            })
-	}
-
-	return state
-}
+export default (state, action) => Object.assign({}, state, {
+    recipes: recipesReducer(state.recipes, action),
+    ingredients: ingredientsReducer(state.ingredients, action),
+})
