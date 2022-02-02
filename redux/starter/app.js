ddd5571ac39d89c7d8504b576850eb198544f105
theNamskov <ahoryaw1@outlook.com>
2022-02-02 22:31:45 +0000

    loaded main ui in application

--- redux/starter/app.js
+++ redux/starter/app.js
@@ -2,6 +2,9 @@
 import store from './store'
 import { addRecipe } from './actions/recipes';
 import { addIngredient } from './actions/ingredients';
+import loadUI from './ui'
+
+loadUI();
 
 store.dispatch(addRecipe('Pancakes'))
 store.dispatch(addIngredient('Pancakes', 'Egg', 3))
